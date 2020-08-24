45198a45cbf36f995fe2106e52c96dd48b0bb0c7
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2020-08-24 19:57:39 +0200

    Revert "Added react-router and fixed line break types using prettier"
    This reverts commit dd905b2d47a389b56e9fed30c3cf9f31d835f085

--- src/app/components/Login/index.tsx
+++ src/app/components/Login/index.tsx
@@ -1,16 +1,5 @@
 import React from 'react';
-import { useHistory } from 'react-router-dom';
-import { Button } from '@adobe/react-spectrum';
 
 export default function Login() {
-	const history = useHistory();
-
-	return (
-		<div>
-			Login
-			<Button onPress={() => history.push('/dashboard')} variant={'cta'}>
-				Login
-			</Button>
-		</div>
-	);
+	return <div>Login</div>;
 }
