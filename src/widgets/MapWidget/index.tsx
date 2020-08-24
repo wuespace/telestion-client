0a8039522e6d3c22467073e30304450335b194eb
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2020-08-24 15:56:54 +0200

    Added interactive map with coordinates from eventbus

--- src/widgets/MapWidget/index.tsx
+++ src/widgets/MapWidget/index.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
-import { Image } from '@adobe/react-spectrum';
+import { CircleMarker, Map, TileLayer } from 'react-leaflet';
 
-import src from './placeholder.png';
+import { MOCK_POSITION } from '../../model/Channels';
+
+import useChannelLatest from '../hooks/useChannelLatest';
+import LoadingIndicator from '../components/LoadingIndicator';
+
+import 'leaflet/dist/leaflet.css';
+import 'leaflet/dist/leaflet.js';
 
 export default function MapWidget() {
-	return <Image src={src} alt="Map" width="100%" height="100%" />;
+	const position = useChannelLatest(MOCK_POSITION);
+	return (
+		<LoadingIndicator dependencies={[position]}>
+			{() => (
+				<Map style={{ height: '100%' }} center={[67.89167, 21.08177]} zoom={6}>
+					<TileLayer
+						attribution='&copy;
+							<a href="https://stadiamaps.com/">
+								Stadia Maps
+							</a>, &copy;
+							<a href="https://openmaptiles.org/">
+								OpenMapTiles
+							</a> &copy;
+							<a href="http://openstreetmap.org">
+								OpenStreetMap
+							</a> contributors'
+						url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
+					/>
+					<CircleMarker center={[position.x, position.y]} radius={10} />
+				</Map>
+			)}
+		</LoadingIndicator>
+	);
 }
