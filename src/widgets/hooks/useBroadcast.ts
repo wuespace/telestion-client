89563a3680b60d2fd29e5f9f2ba496f0a044090d
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2020-08-28 20:17:08 +0200

    Added documentation and updated overview

--- src/widgets/hooks/useBroadcast.ts
+++ src/widgets/hooks/useBroadcast.ts
@@ -1,3 +1,9 @@
+/**
+ * A react hook that enables broadcast functionality via the event bus.
+ * @packageDocumentation
+ *
+ * @category Widget Hook
+ */
 import { useCallback } from 'react';
 
 import { Channel } from '../../model/Channels';
@@ -5,6 +11,23 @@ import JSONSerializable from '../../model/JSONSerializable';
 
 import useEventBus from './useEventBus';
 
+/**
+ * Send a broadcast on the specified channel with the returned function.
+ * @param channel channel to broadcast to
+ * @returns a function that broadcast a message
+ *
+ * @see {@link EventBus.publish}
+ *
+ * @example ```ts
+ * const broadcast = useBroadcast('channel:hello-world');
+ *
+ * const handleClick = () => {
+ *   broadcast('Hello World');
+ * };
+ *
+ * return <button onClick={handleClick}>Broadcast a hello world</button>;
+ * ```
+ */
 export default function useBroadcast(channel: Channel) {
 	const eventBus = useEventBus();
 
--- src/widgets/hooks/useRequest.ts
+++ src/widgets/hooks/useRequest.ts
@@ -1,14 +1,58 @@
+/**
+ * A react hook that enables send and reply functionality via the event bus.
+ * @packageDocumentation
+ *
+ * @category Widget Hook
+ */
 import { useCallback } from 'react';
 
 import { Channel } from '../../model/Channels';
 import JSONSerializable from '../../model/JSONSerializable';
-import { Callback } from '../../model/VertxEventBus';
+import { Callback, ErrorMessage } from '../../model/VertxEventBus';
 
 import useEventBus from './useEventBus';
 
+/**
+ * Send a message on the specified channel
+ * and get the first reply via a callback.
+ * @param channel channel to send to
+ * @returns a function that send a message and register a one-time callback
+ *
+ * @see {@link EventBus.send}
+ *
+ * @example ```ts
+ * const [answer, setAnswer] = useState<JSONSerializable>();
+ * const send = useRequest('channel:send-my-anything');
+ *
+ * const handleClick = () => {
+ *   send('Ping', (message: JSONSerializable) => {
+ *     if (message) setAnswer(message);
+ *   });
+ * };
+ *
+ * return (
+ *   <>
+ *     <button onClick={handleClick}>Send message</button>
+ *     <p>{answer}</p>
+ *   </>
+ * );
+ * ```
+ */
 export default function useRequest(channel: Channel) {
 	const eventBus = useEventBus();
-	return useCallback((message: JSONSerializable, callback: Callback) => {
-		eventBus.send(channel, message, callback);
-	}, [channel, eventBus]);
+	return useCallback(
+		(
+			message: JSONSerializable,
+			callback: (
+				message: JSONSerializable | null,
+				error: ErrorMessage | null
+			) => void
+		) => {
+			const cb: Callback = (message, error) => {
+				callback(message ? message.body : null, error);
+			};
+			eventBus.send(channel, message, cb);
+		},
+		[channel, eventBus]
+	);
 }
