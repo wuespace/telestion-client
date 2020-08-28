f7804f3581698b752f426a082e76e94aaa118631
Pablo Klaschka <contact@pabloklaschka.de>
2020-08-28 16:01:41 +0200

    Fixed line separators

--- src/model/Channels.ts
+++ src/model/Channels.ts
@@ -1,4 +1,5 @@
-export const MOCK_POSITION = 'org.telestion.core.verticle.RandomPositionPublisher/out#MockPos';
+export const MOCK_POSITION =
+	'org.telestion.core.verticle.RandomPositionPublisher/out#MockPos';
 export const EXAMPLE_CHANNEL = 'HEY';
 
 export type Channel = typeof MOCK_POSITION | typeof EXAMPLE_CHANNEL;
