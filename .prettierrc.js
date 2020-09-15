b2cea2515350bc4ad0ab26dcfaa98b267af5b456
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2020-09-15 19:18:41 +0200

    Configured cypress and added project scripts

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,9 +1,9 @@
 module.exports = {
-  printWidth: 80,
+	printWidth: 80,
 	tabWidth: 2,
 	useTabs: true,
 	semi: true,
 	singleQuote: true,
 	trailingComma: 'none',
-	arrowParens: 'avoid',
+	arrowParens: 'avoid'
 };
