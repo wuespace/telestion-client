00430280cbde79b51311816bdb96ae78e4b6c87f
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-01-04 15:04:33 +0100

    chore(ci): Lint Action (6)

--- packages/telestion-client-core/.eslintrc.js
+++ packages/telestion-client-core/.eslintrc.js
@@ -1,3 +1,52 @@
 module.exports = {
-	extends: ['../../.eslintrc.react.js']
+	env: {
+		browser: true,
+		es2020: true
+	},
+	plugins: ['react', '@typescript-eslint', 'jest', 'prettier'],
+	extends: [
+		'airbnb-typescript',
+		'airbnb/hooks',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended',
+		'plugin:jest/recommended',
+		'prettier',
+		'prettier/react',
+		'prettier/@typescript-eslint',
+		'plugin:prettier/recommended'
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true
+		},
+		ecmaVersion: 11,
+		sourceType: 'module',
+		project: './tsconfig.json'
+	},
+	rules: {
+		// Too restrictive, writing ugly code to defend against a very unlikely scenario: https://eslint.org/docs/rules/no-prototype-builtins
+		'no-prototype-builtins': 'off',
+		// https://basarat.gitbooks.io/typescript/docs/tips/defaultIsBad.html
+		'import/prefer-default-export': 'off',
+		'import/no-default-export': 'error',
+		// Too restrictive: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/destructuring-assignment.md
+		'react/destructuring-assignment': 'off',
+		// No jsx extension: https://github.com/facebook/create-react-app/issues/87#issuecomment-234627904
+		'react/jsx-filename-extension': 'off',
+		// Use function hoisting to improve code readability
+		'no-use-before-define': [
+			'error',
+			{ functions: false, classes: true, variables: true }
+		],
+		// Makes no sense to allow type inference for expression parameters, but require typing the response
+		'@typescript-eslint/explicit-function-return-type': [
+			'error',
+			{ allowExpressions: true, allowTypedFunctionExpressions: true }
+		],
+		'@typescript-eslint/no-use-before-define': [
+			'error',
+			{ functions: false, classes: true, variables: true, typedefs: true }
+		]
+	}
 };
