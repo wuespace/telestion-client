aea9f0580bbbb062bf43e5458b464b69dbd1adfe
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-01-06 00:59:24 +0100

    feat(mock-server): Configured package build and lint

---
+++ packages/mock-server/rollup.config.js
@@ -0,0 +1,6 @@
+import { join } from 'path';
+import buildConfig from '../../base-configs/rollup.config.base';
+
+const packageJson = require('./package.json');
+
+export default buildConfig(join(process.cwd(), 'src', 'index.ts'), packageJson);
