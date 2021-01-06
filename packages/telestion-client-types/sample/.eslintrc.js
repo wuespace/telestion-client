5170c21c33134f55c51144c22042b22c8096152a
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-01-06 10:04:11 +0100

    chore(types): Fix ESLint configuration for samples

--- packages/telestion-client-types/sample/.eslintrc.js
+++ packages/telestion-client-types/sample/.eslintrc.js
@@ -1,19 +1,29 @@
-module.exports = {
-	root: true
-};
+const path = require('path');
 
-// // eslint-disable-next-line @typescript-eslint/no-var-requires
-// const path = require('path');
-//
-// module.exports = {
-// 	extends: ['../../.eslintrc.js'],
-// 	parserOptions: {
-// 		ecmaVersion: 11,
-// 		sourceType: 'module',
-// 		project: [path.join(__dirname, 'tsconfig.json')]
-// 	},
-// 	rules: {
-// 		'@typescript-eslint/no-namespace': 'off',
-// 		'@typescript-eslint/no-unused-vars': 'off'
-// 	}
-// };
+module.exports = {
+	root: true,
+	globals: {
+		// global TypeScript namespace
+		TelestionClient: 'readonly'
+	},
+	parserOptions: {
+		tsconfigRootDir: __dirname,
+		project: [path.join(__dirname, 'tsconfig.json')]
+	},
+	extends: [
+		path.join(
+			__dirname,
+			'..',
+			'..',
+			'..',
+			'base-configs',
+			'eslint.typescript.js'
+		)
+	],
+	rules: {
+		'no-unused-vars': 'off',
+		'@typescript-eslint/no-namespace': 'off',
+		'@typescript-eslint/no-unused-vars': 'off'
+	},
+	ignorePatterns: ['sample', 'types']
+};
