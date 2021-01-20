18de1910ea08e79baccf1c64d9e0503d59db49aa
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-01-20 15:20:20 +0100

    chore(build): Add separate TS build config to fix declaration generation issue

--- base-configs/rollup.config.base.js
+++ base-configs/rollup.config.base.js
@@ -34,7 +34,7 @@ export default function buildConfig(inputPath, packageJson) {
 			peerDepsExternal(),
 			resolve({ preferBuiltins: true }),
 			commonjs(),
-			typescript({ useTsconfigDeclarationDir: true }),
+			typescript({ tsconfig: 'tsconfig.build.json' }),
 			terser()
 		],
 		external: [
