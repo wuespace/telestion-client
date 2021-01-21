35b6ba8d5c89e5c9e089ca2d9b6882caa7301588
Pablo Klaschka <contact@pabloklaschka.de>
2021-01-21 21:51:25 +0100

    refactor(core): Extract models of state hooks into their own files
    This reduces the otherwise rather large file length, in turn increasing code readability

--- packages/telestion-client-core/src/hooks/stores/index.ts
+++ packages/telestion-client-core/src/hooks/stores/index.ts
@@ -2,3 +2,6 @@ export * from './use-auth';
 export * from './use-event-bus';
 export * from './use-notifications';
 export * from './use-preferences';
+export * from './use-preferences.model';
+export * from './use-notification.model';
+export * from './use-event-bus.model';
