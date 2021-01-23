b0641485e3da74c67b35723d19bfbd48a3cf45ab
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-01-23 14:15:19 +0100

    fix(common): Finished wrapper component

--- packages/telestion-client-common/src/components/common-wrapper.tsx
+++ packages/telestion-client-common/src/components/common-wrapper.tsx
@@ -1,24 +1,46 @@
 import { ReactElement } from 'react';
+import { StateSelector } from 'zustand';
 import { defaultTheme, Provider, Flex } from '@adobe/react-spectrum';
+import { Widget } from '@wuespace/telestion-client-types';
+
 import { AppLogoContext } from './contexts/app-logo-context';
+import { WidgetsContext } from './contexts/widgets-context';
+import { ColorScheme, ColorSchemeState, useColorScheme } from '../hooks';
+
+// color scheme selector
+const selector: StateSelector<ColorSchemeState, ColorScheme> = ({
+	colorScheme
+}) => colorScheme;
 
 export interface CommonWrapperProps {
 	appLogo?: string;
+	widgets?: Array<Widget>;
 	children: ReactElement | ReactElement[];
 }
 
-export function CommonWrapper({ appLogo, children }: CommonWrapperProps) {
+export function CommonWrapper({
+	appLogo,
+	widgets,
+	children
+}: CommonWrapperProps) {
+	const colorScheme = useColorScheme(selector);
+
 	return (
-		<Provider theme={defaultTheme} colorScheme="dark">
+		<Provider
+			theme={defaultTheme}
+			colorScheme={colorScheme !== 'system' ? colorScheme : undefined}
+		>
 			<AppLogoContext appLogo={appLogo}>
-				<Flex
-					direction="column"
-					height="100vh"
-					justifyContent="start"
-					alignItems="center"
-				>
-					{children}
-				</Flex>
+				<WidgetsContext widgets={widgets}>
+					<Flex
+						direction="column"
+						height="100vh"
+						justifyContent="start"
+						alignItems="center"
+					>
+						{children}
+					</Flex>
+				</WidgetsContext>
 			</AppLogoContext>
 		</Provider>
 	);
