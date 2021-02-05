2cac521df6fb45e43ca3644856182e04bd15e7e3
Pablo Klaschka <contact@pabloklaschka.de>
2021-02-05 18:28:32 +0100

    refactor(cli): Extract lib method `openUrl` from `docs` command

--- packages/telestion-client-cli/src/commands/docs.js
+++ packages/telestion-client-cli/src/commands/docs.js
@@ -1,6 +1,6 @@
+const openUrl = require('../lib/open-url');
 const debug = require('debug')('docs');
 const logger = require('../lib/logger')('docs');
-const spawn = require('child_process').spawn;
 
 // yargs def
 const command = ['docs', 'd'];
@@ -13,25 +13,8 @@ function builder(yargs) {
 async function handler(argv) {
 	// gathering information
 	debug('Arguments:', argv);
-
-	let command = '';
-
-	switch (process.platform) {
-		case 'darwin':
-			command = 'open';
-			break;
-		case 'win32':
-			command = 'explorer.exe';
-			break;
-		case 'linux':
-			command = 'xdg-open';
-			break;
-		default:
-			throw new Error('Unsupported platform: ' + process.platform);
-	}
-
 	logger.info('Opening documentation in system browser');
-	spawn(command, ['https://telestionteam.github.io/telestion-client/']);
+	openUrl('https://telestionteam.github.io/telestion-client/');
 	logger.success('Document opened successfully');
 }
 
