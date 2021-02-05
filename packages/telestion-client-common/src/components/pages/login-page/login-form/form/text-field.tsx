72b707698775bf534832394260a486149898adf4
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-02-05 03:08:28 +0100

    docs(common): Document text field component for the login page
    Refs: #272

--- packages/telestion-client-common/src/components/pages/login-page/login-form/form/text-field.tsx
+++ packages/telestion-client-common/src/components/pages/login-page/login-form/form/text-field.tsx
@@ -2,19 +2,74 @@
 import { useEffect, useState } from 'react';
 import { TextField as SpectrumTextField } from '@adobe/react-spectrum';
 import { SpectrumTextFieldProps } from '@react-types/textfield';
+
 import { ValidationState } from './validate-inputs';
 
+/**
+ * React Props of {@link TextField}
+ *
+ * For more information about React Props, please look here:
+ * {@link https://reactjs.org/docs/components-and-props.html}
+ *
+ * @see {@link TextField}
+ * @see {@link https://reactjs.org/docs/components-and-props.html}
+ */
 export type TextFieldProps = Omit<
 	SpectrumTextFieldProps,
 	'onChange' | 'value' | 'defaultValue'
 > & {
+	/**
+	 * The initial value of the text field.
+	 * If it not defined the text field is initially empty.
+	 */
 	initialValue?: string;
 
+	/**
+	 * An event that triggers on every input change
+	 * and reports the current text field content.
+	 *
+	 * If the current input is invalid
+	 * (definable by the {@link TextFieldProps.validator} function)
+	 * the reported value is `null`.
+	 *
+	 * @param value - the current text field content
+	 */
 	onChange?: (value: string | null) => void;
 
+	/**
+	 * The validator function that "tests" the current content if it is valid.
+	 * @param text - the current content of the text field to test
+	 * @returns the validation state of the tested content
+	 */
 	validator?: (text: string) => ValidationState;
 };
 
+/**
+ * The text field component that renders a react spectrum text field
+ * and is typically used a form.
+ *
+ * It can test the input with a given validator function
+ * to indicate correctness of the current input.
+ *
+ * @see {@link TextFieldProps}
+ *
+ * @example
+ * ```ts
+ * function MyForm() {
+ * 	const [username, setUsername] = useState<string | null>('');
+ *
+ * 	return (
+ * 		<TextField
+ * 			label="Username"
+ * 			placeholder="Your username"
+ * 			initialValue={initialUsername}
+ * 			onChange={setUsername}
+ * 			validator={isValidText}
+ * 		/>
+ * 	);
+ * }
+ * ```
+ */
 export function TextField({
 	initialValue = '',
 	onChange,
