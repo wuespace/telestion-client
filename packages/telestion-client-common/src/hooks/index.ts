6684ede2d10a08ffe5e5f66ce867a564f6ed24f4
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-02-05 01:44:51 +0100

    feat(common): Add hook for managing dependency timeouts
    Refs: #261

--- packages/telestion-client-common/src/hooks/index.ts
+++ packages/telestion-client-common/src/hooks/index.ts
@@ -1,2 +1,4 @@
 export * from './abstractions';
 export * from './stores';
+
+export * from './use-dependency-timeout';
---
+++ packages/telestion-client-common/src/hooks/use-dependency-timeout.ts
@@ -0,0 +1,55 @@
+import { useEffect, useState } from 'react';
+
+/**
+ * Throws an error after a timeout if any dependency is undefined.
+ * @param dependencies - the dependencies to check if they are undefined
+ * @param timeout - the timeout in milliseconds until the function throws,
+ * if it is 0 it waits indefinitely
+ *
+ * @throws Error - if no data is received until timeout
+ *
+ * @example
+ * ```ts
+ * const [position, setPosition] = useState<Position>();
+ *
+ * // throws if no position received after 5 seconds
+ * useDependencyTimeout(5, [position]);
+ *
+ * return <p>Latest position: {position}</p>;
+ * ```
+ */
+export function useDependencyTimeout(
+	timeout: number,
+	dependencies: any[]
+): boolean {
+	const [isUndefined, setIsUndefined] = useState(true);
+	const [error, setError] = useState<Error>();
+
+	useEffect(() => {
+		let id: any;
+		if (timeout > 0 && isUndefined) {
+			id = setTimeout(() => {
+				setError(new Error('Timeout exceeded without any data'));
+			}, timeout);
+		}
+
+		return () => {
+			if (id) {
+				clearTimeout(id);
+			}
+		};
+	}, [isUndefined, timeout]);
+
+	useEffect(() => {
+		setIsUndefined(
+			dependencies.some(dependency => typeof dependency === 'undefined')
+		);
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [...dependencies]);
+
+	if (error) {
+		throw error;
+	}
+
+	return isUndefined;
+}
