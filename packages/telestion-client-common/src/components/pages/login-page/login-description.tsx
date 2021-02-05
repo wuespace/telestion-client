953ad14a778d4aa29b313f267a2c786358523bf6
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-02-05 03:21:55 +0100

    docs(common): Document login description component for the login page
    Refs: #272

--- packages/telestion-client-common/src/components/pages/login-page/login-description.tsx
+++ packages/telestion-client-common/src/components/pages/login-page/login-description.tsx
@@ -1,12 +1,63 @@
-import { Text } from '@adobe/react-spectrum';
 import { ReactElement } from 'react';
+import { Text } from '@adobe/react-spectrum';
 
+/**
+ * React Props of {@link LoginDescription}
+ *
+ * For more information about React Props, please look here:
+ * {@link https://reactjs.org/docs/components-and-props.html}
+ *
+ * @see {@link LoginDescription}
+ * @see {@link https://reactjs.org/docs/components-and-props.html}
+ */
 export interface LoginDescriptionProps {
+	/**
+	 * The alternative description to use instead of the default one.
+	 */
 	description?: string;
 
+	/**
+	 * Components that completely replace the description
+	 * and are rendered instead.
+	 */
 	children?: ReactElement | Array<ReactElement>;
 }
 
+/**
+ * The login description component that renders the current login description
+ * in the login page.
+ *
+ * It defaults to:
+ *
+ * _Please enter the credentials assigned to you by the Ground Station team_
+ *
+ * and can be overridden by the prop {@link LoginDescriptionProps.description}
+ * or can be completely replaced by another component via defined children.
+ *
+ * **Attention**
+ *
+ * If children are specified, the overridden description is completely ignored
+ * and the children are rendered instead!
+ *
+ * This component belongs to the {@link LoginPage}
+ *
+ * @see {@link LoginDescriptionProps}
+ * @see {@link LoginPage}
+ *
+ * @example
+ * ```ts
+ * function MyLoginPage() {
+ * 	return (
+ * 		<LoginPage>
+ * 			<LoginTitle />
+ * 			<LoginLogo />
+ * 			<LoginDescription />
+ * 			<LoginForm initialServerURL="http://localhost:9870/bridge" />
+ * 		</LoginPage>
+ * 	);
+ * }
+ * ```
+ */
 export function LoginDescription({
 	description,
 	children
