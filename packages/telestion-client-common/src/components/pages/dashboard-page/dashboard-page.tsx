62c7351782e7f95a3dab0c16c2fb3c06fd839b00
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-02-06 03:15:39 +0100

    docs(common): Document all page components

--- packages/telestion-client-common/src/components/pages/dashboard-page/dashboard-page.tsx
+++ packages/telestion-client-common/src/components/pages/dashboard-page/dashboard-page.tsx
@@ -6,6 +6,45 @@ import { Dashboard } from './dashboard/dashboard';
 import { NoDashboardsMessage } from './no-dashboards-message';
 import { useCurrentDashboards } from '../../../hooks';
 
+/**
+ * A Telestion Client page that renders a dashboard page.
+ * It displays the currently active dashboard of an authenticated user.
+ *
+ * It can be changed with the {@link DashboardPicker} component
+ * usually used in the application header.
+ * To register new dashboard for the user or change existing,
+ * please use the {@link useUserConfig} hook.
+ *
+ * It only renders if someone is authenticated.
+ * Otherwise, it redirects to the login page.
+ *
+ * **Attention**
+ *
+ * If you create your own dashboard page, please pass through
+ * the routing information so the pages component read it
+ * and render the page the right way:
+ * `MyDashboardPage.routing = TCDashboardPage.routing;`
+ *
+ * @see {@link DashboardPicker}
+ * @see {@link useUserConfig}
+ * @see {@link @wuespace/telestion-client-types#UserConfig}
+ * @see {@link @wuespace/telestion-client-core#Pages}
+ *
+ * @example
+ * ```ts
+ * function App() {
+ * 	return (
+ * 		<TelestionClient>
+ * 			<Pages>
+ * 				<LoginPage />
+ * 				<DashboardPage />
+ * 				<NotFoundPage />
+ * 			</Pages>
+ * 		</TelestionClient>
+ * 	);
+ * }
+ * ```
+ */
 export function DashboardPage() {
 	const { id } = useParams<{ id: string }>();
 	const [dashboards] = useCurrentDashboards();
@@ -19,6 +58,12 @@ export function DashboardPage() {
 	return <Dashboard dashboard={dashboards[idAsNumber]} />;
 }
 
+/**
+ * The routing for the dashboard page of Telestion Client Common package.
+ *
+ * It only renders if someone is authenticated.
+ * Otherwise, it redirects to the login page.
+ */
 const routing: Routing = {
 	type: 'auth',
 	path: '/dashboard/:id',
--- packages/telestion-client-common/src/components/pages/not-found-page/not-found-page.tsx
+++ packages/telestion-client-common/src/components/pages/not-found-page/not-found-page.tsx
@@ -1,10 +1,48 @@
 import { Routing } from '@wuespace/telestion-client-types';
 import { NotFoundMessage } from './not-found-message';
 
+/**
+ * A Telestion Client page that renders a fallback page
+ * if the current route does not fit to a page higher up in the list.
+ * It displays a "not found" message and a button to return
+ * to the dashboard page if the some is authenticated.
+ * Otherwise it returns to the login page.
+ *
+ * It renders always regardless if someone is authenticated or not.
+ *
+ * **Attention**
+ *
+ * If you create your own not found page, please pass through
+ * the routing information so the pages component read it
+ * and render the page the right way:
+ * `MyNotFoundPage.routing = TCNotFoundPage.routing;`
+ *
+ * @see {@link @wuespace/telestion-client-core#Pages}
+ *
+ * @example
+ * ```ts
+ * function App() {
+ * 	return (
+ * 		<TelestionClient>
+ * 			<Pages>
+ * 				<LoginPage />
+ * 				<DashboardPage />
+ * 				<NotFoundPage />
+ * 			</Pages>
+ * 		</TelestionClient>
+ * 	);
+ * }
+ * ```
+ */
 export function NotFoundPage() {
 	return <NotFoundMessage />;
 }
 
+/**
+ * The routing for the not found page of Telestion Client Common package.
+ *
+ * It renders always regardless if someone is authenticated or not.
+ */
 const routing: Routing = {
 	type: 'default',
 	path: '/',
