a28f5e8a947cda7e8d24573e21b0b761cbdc89f4
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-02-06 01:15:37 +0100

    docs(common): Document the action divider component

--- packages/telestion-client-common/src/components/header/actions/action-divider.tsx
+++ packages/telestion-client-common/src/components/header/actions/action-divider.tsx
@@ -1,5 +1,33 @@
 import { Divider } from '@adobe/react-spectrum';
 
+/**
+ * Part of the header actions.
+ *
+ * This is a special action
+ * where the actions around this action divided via a vertical bar.
+ *
+ * The action is useful the group registered actions into subgroup
+ * for a better overview.
+ *
+ * This is also specified in the Adobe Spectrum Design:
+ * https://spectrum.adobe.com/page/headers/#Collapsing-Action-Buttons
+ *
+ * @see {@link https://spectrum.adobe.com/page/headers/#Collapsing-Action-Buttons}
+ * @see {@link Actions}
+ *
+ * @example
+ * ```ts
+ * function MyActions() {
+ * 	return (
+ * 		<Actions>
+ * 			<NotificationAction />
+ * 			<ActionDivider />
+ * 			<ColorSchemeAction />
+ * 		</Actions>
+ * 	);
+ * }
+ * ```
+ */
 export function ActionDivider() {
 	return <Divider orientation="vertical" size="S" />;
 }
