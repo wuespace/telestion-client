4d373e7dc9e1131da7c993b21a66b96a8fe08475
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-02-06 01:31:57 +0100

    docs(common): Document the account controls component

--- packages/telestion-client-common/src/components/header/account-controls/account-controls.tsx
+++ packages/telestion-client-common/src/components/header/account-controls/account-controls.tsx
@@ -4,6 +4,32 @@ import { AvatarButton } from './avatar-button';
 import { AvatarMenu } from './avatar-menu';
 import { StatusDialog } from './status-dialog';
 
+/**
+ * Part of the Telestion Client Common header.
+ *
+ * This header component lets the user review
+ * and control his login and authentication status.
+ *
+ * It displays the current login status
+ * as well as some further information about the event bus state.
+ *
+ * It renders a simple avatar icon with a dropdown menu attached to it.
+ *
+ * @see {@link Header}
+ *
+ * @example
+ * ```ts
+ * function AppHeader() {
+ * 	return (
+ * 		<Header
+ * 			right={
+ * 				<AccountControls />
+ * 			}
+ * 		/>
+ * 	);
+ * }
+ * ```
+ */
 export function AccountControls() {
 	// dialog open state
 	const [isOpen, setOpen] = useState(false);
