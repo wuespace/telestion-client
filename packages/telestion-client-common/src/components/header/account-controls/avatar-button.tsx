c8a0c516edcd830c04d81655ee0581c9d5874fef
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-02-06 01:50:28 +0100

    docs(common): Document the avatar button component

--- packages/telestion-client-common/src/components/header/account-controls/avatar-button.tsx
+++ packages/telestion-client-common/src/components/header/account-controls/avatar-button.tsx
@@ -1,6 +1,27 @@
 import { View, ActionButton } from '@adobe/react-spectrum';
 import defaultAvatar from '../../../media/default-avatar.png';
 
+/**
+ * Renders an action button with a placeholder avatar image inside.
+ *
+ * Typically used with the {@link AvatarMenu} in a
+ * {@link @adobe/react-spectrum#MenuTrigger}
+ * to control the menu with the avatar button.
+ *
+ * @see {@link AvatarMenu}
+ *
+ * @example
+ * ```ts
+ * function MyAccountControls() {
+ * 	return (
+ * 		<MenuTrigger>
+ * 			<AvatarButton />
+ * 			<AvatarMenu />
+ * 		</MenuTrigger>
+ * 	);
+ * }
+ * ```
+ */
 export function AvatarButton() {
 	return (
 		<ActionButton>
