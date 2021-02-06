db0ea7b9f2525e327559e9b9695ed85dba57f08c
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-02-06 03:29:44 +0100

    docs(common): Document the overflow fix component

--- packages/telestion-client-common/src/components/pages/dashboard-page/dashboard/overflow-fix.tsx
+++ packages/telestion-client-common/src/components/pages/dashboard-page/dashboard/overflow-fix.tsx
@@ -3,10 +3,42 @@ import { ReactNode } from 'react';
 import { View } from '@adobe/react-spectrum';
 import { ViewProps } from '@react-types/view';
 
+/**
+ * React Props of {@link OverflowFix}
+ *
+ * For more information about React Props, please look here:
+ * {@link https://reactjs.org/docs/components-and-props.html}
+ *
+ * @see {@link OverflowFix}
+ * @see {@link https://reactjs.org/docs/components-and-props.html}
+ */
 export interface OverflowFixProps extends ViewProps {
+	/**
+	 * The components which are possibly overflow.
+	 */
 	children: ReactNode;
 }
 
+/**
+ * Prevents that the children of this component overflow
+ * and break the layout of the application.
+ *
+ * In the background it uses the {@link @adobe/react-spectrum#View} component.
+ * All props of this component are passed through to the outer view container.
+ *
+ * {@link @adobe/react-spectrum#View}
+ *
+ * @example
+ * ```ts
+ * function MyComponent() {
+ * 	return (
+ * 	  <OverflowFix borderRadius="regular">
+ * 	  	<IWillDefinitelyOverflow />
+ * 	  </OverflowFix>
+ * 	); // not anymore :D
+ * }
+ * ```
+ */
 export function OverflowFix({ children, ...viewProps }: OverflowFixProps) {
 	return (
 		<View width="100%" height="100%" position="relative" {...viewProps}>
