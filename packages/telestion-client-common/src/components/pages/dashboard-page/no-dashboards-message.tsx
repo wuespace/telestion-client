2b0c00594559630a689c1d11dcbe7bea9cf52d70
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-02-06 03:23:11 +0100

    docs(common): Document the no dashboards message component

--- packages/telestion-client-common/src/components/pages/dashboard-page/no-dashboards-message.tsx
+++ packages/telestion-client-common/src/components/pages/dashboard-page/no-dashboards-message.tsx
@@ -1,6 +1,29 @@
 import { IllustratedMessage, Heading, Content } from '@adobe/react-spectrum';
 import NotFound from '@spectrum-icons/illustrations/NotFound';
 
+/**
+ * Renders an illustrated message from React Spectrum
+ * containing a no dashboards heading.
+ * It indicates that a user has no dashboards created yet.
+ *
+ * It is usually used in the {@link DashboardPage}.
+ *
+ * @see {@link @adobe/react-spectrum#IllustratedMessage}
+ * @see {@link DashboardPage}
+ *
+ * @example
+ * ```ts
+ * function MyDashboardPage() {
+ * 	const [dashboards] = useCurrentDashboards();
+ *
+ * 	if (!dashboards) {
+ * 		return <NoDashboardsMessage />;
+ * 	}
+ *
+ * 	return <Dashboard dashboard={dashboards[0]} />;
+ * }
+ * ```
+ */
 export function NoDashboardsMessage() {
 	return (
 		<IllustratedMessage>
