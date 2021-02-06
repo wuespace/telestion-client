9dee2d3d3af3ea46c02a1597baed0dc353dc321a
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-02-06 03:19:02 +0100

    docs(common): Document the not found message component

--- packages/telestion-client-common/src/components/pages/not-found-page/not-found-message.tsx
+++ packages/telestion-client-common/src/components/pages/not-found-page/not-found-message.tsx
@@ -4,6 +4,24 @@ import { Heading, IllustratedMessage, Content } from '@adobe/react-spectrum';
 
 import NotFound from '@spectrum-icons/illustrations/NotFound';
 
+/**
+ * Renders an illustrated message from React Spectrum
+ * containing a 404 page not found heading and a button to return
+ * to the dashboard page if the some is authenticated.
+ * Otherwise it returns to the login page.
+ *
+ * It is usually used in the {@link NotFoundPage}.
+ *
+ * @see {@link @adobe/react-spectrum#IllustratedMessage}
+ * @see {@link NotFoundPage}
+ *
+ * @example
+ * ```ts
+ * function MyNotFoundPage() {
+ * 	return <NotFoundMessage />;
+ * }
+ * ```
+ */
 export function NotFoundMessage() {
 	const auth = useAuth(state => state.auth);
 
