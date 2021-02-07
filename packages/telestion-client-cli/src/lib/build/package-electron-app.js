88e2e15000e9c1dd0d477fe9bffb0f255150762e
Pablo Klaschka <contact@pabloklaschka.de>
2021-02-07 02:56:48 +0100

    feat(cli): Show files generated by electron-builder in `tc-cli build`

--- packages/telestion-client-cli/src/lib/build/package-electron-app.js
+++ packages/telestion-client-cli/src/lib/build/package-electron-app.js
@@ -1,7 +1,7 @@
 /**
  * Packages the app when called with process.cwd() as the PSC root directory.
  *
- * @return {Promise<string>} A list of produced artifacts
+ * @return {Promise<string[]>} A list of produced artifacts
  */
 function packageElectronApp() {
 	const electronBuilder = require('electron-builder');
