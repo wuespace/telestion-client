f2760e3f0c377c2cc9c9b409d71460db68927581
Pablo Klaschka <contact@pabloklaschka.de>
2021-02-07 01:12:17 +0100

    feat(cli)!: Add configuration file parsing to the CLI
    BREAKING CHANGE: PSC's now require a `telestion.config.js` file in the PSC's root directory (next to the `package.json`).
    
      To migrate PSCs generated with older versions of the CLI, please add a `telestion.config.js` file with the following content into the root PSC directory, next to the `package.json`:
      ```js
      module.exports = {}
      ```

---
+++ packages/telestion-client-cli/src/lib/config.js
@@ -0,0 +1,24 @@
+const { cosmiconfig } = require('cosmiconfig');
+
+const moduleName = 'telestion';
+const explorer = cosmiconfig(moduleName, {
+	ignoreEmptySearchPlaces: false
+});
+
+/**
+ * @returns {Promise<import('cosmiconfig').CosmiconfigResult>}
+ */
+async function getConfig() {
+	const config = await explorer.search();
+
+	if (config === null) {
+		throw new Error(
+			'No config file found. Expected to find .telestionrc, telestion.config.js, or something similar. ' +
+				'Please create a config file and try again.'
+		);
+	}
+
+	return config;
+}
+
+module.exports = getConfig;
