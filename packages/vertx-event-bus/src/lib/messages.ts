1687298fad154b82528e8a16c957015884bc70fc
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-04-05 00:32:26 +0200

    test(event-bus): Define tests for Vert.x Event Bus

--- packages/vertx-event-bus/src/lib/messages.ts
+++ packages/vertx-event-bus/src/lib/messages.ts
@@ -10,10 +10,34 @@ import {
 	UnregisterMessage
 } from '@wuespace/telestion-client-types';
 
+/**
+ * Builds a ping message.
+ *
+ * @example
+ * ```ts
+ * this.pingTimer = setInterval(
+ * 	() => this.sandRaw(pingMessage()),
+ * 	this.options.pingInterval
+ * );
+ * ```
+ */
 export function pingMessage(): PingMessage {
 	return { type: 'ping' };
 }
 
+/**
+ * Builds a register message.
+ * @param address - the address which should be registered
+ * @param headers - additional headers sent with the message
+ * @returns a valid register message
+ *
+ * @example
+ * ```ts
+ * if (this.handlers.every(handler => handler[0] !== address)) {
+ * 	this.bus.send(registerMessage(address, headers), false);
+ * }
+ * ```
+ */
 export function registerMessage(
 	address: ChannelAddress,
 	headers: Headers = {}
@@ -25,6 +49,19 @@ export function registerMessage(
 	};
 }
 
+/**
+ * Builds an unregister message.
+ * @param address - the address which should be unregistered
+ * @param headers - additional headers sent with the message
+ * @returns a valid unregister message
+ *
+ * @example
+ * ```ts
+ * if (this.handlers.every(handler => handler[0] !== address)) {
+ * 	this.bus.send(unregisterMessage(address, headers), false);
+ * }
+ * ```
+ */
 export function unregisterMessage(
 	address: ChannelAddress,
 	headers: Headers = {}
@@ -36,6 +73,18 @@ export function unregisterMessage(
 	};
 }
 
+/**
+ * Builds a publish message.
+ * @param address - the address of the message
+ * @param content - the content to publish
+ * @param headers - additional headers sent with the message
+ * @returns a valid publish message
+ *
+ * @example
+ * ```ts
+ * this.bus.send(publishMessage(address, content, headers));
+ * ```
+ */
 export function publishMessage(
 	address: ChannelAddress,
 	content: JsonSerializable,
@@ -49,6 +98,19 @@ export function publishMessage(
 	};
 }
 
+/**
+ * Builds a send message.
+ * @param address - the address of the message
+ * @param replyAddress - the reply address of the message
+ * @param content - the content to send
+ * @param headers - additional headers sent with the message
+ * @returns a valid send message
+ *
+ * @example
+ * ```ts
+ * this.bus.send(sendMessage(address, replyAddress, content, headers));
+ * ```
+ */
 export function sendMessage(
 	address: ChannelAddress,
 	replyAddress: ChannelAddress,
@@ -64,6 +126,22 @@ export function sendMessage(
 	};
 }
 
+/**
+ * Builds an error message.
+ * @param failureCode - the failure code of the error
+ * @param failureType - the failure type of the error
+ * @param message - additional information on this error
+ * @returns a valid error message
+ *
+ * @example
+ * ```ts
+ * try {
+ * 	// something
+ * } catch (err) {
+ * 	this.onMessage?.(errorMessage(1, 'Error', 'Something happened'));
+ * }
+ * ```
+ */
 export function errorMessage(
 	failureCode: number,
 	failureType: string,
