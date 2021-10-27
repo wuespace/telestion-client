6ae16e2196e4d2fcb13a62b5b0eda084e827e444
Ludwig Richter <ludwig.richter@stud-mail.uni-wuerzburg.de>
2021-10-27 11:23:46 +0200

    refactor(cli): Fix webpack loader for global plugins

--- packages/telestion-client-cli/src/lib/build/custom-webpack-loader/electron-main-import-plugins.js
+++ packages/telestion-client-cli/src/lib/build/custom-webpack-loader/electron-main-import-plugins.js
@@ -1,5 +1,3 @@
-const { getOptions } = require('loader-utils');
-
 /**
  * A custom webpack loader that fills a global constant `PLUGINS: Plugin[]` with the plugins provided by the
  * {@link webpack.ProvidePlugin} as `PLUGIN_0`, `PLUGIN_1`, ...
@@ -8,7 +6,7 @@ const { getOptions } = require('loader-utils');
  * @return {string} the modified source code
  */
 function GlobalPluginsWebpackLoader(source) {
-	const options = getOptions(this);
+	const options = this.getOptions();
 
 	return `const PLUGINS = [${options.plugins
 		.map((path, index) => `PLUGIN_${index}`)
