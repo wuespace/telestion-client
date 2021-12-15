1a30c165998c62c773a57414c290c2cb5afada23
Ludwig Richter <ludwig.richter@wuespace.de>
2021-12-15 01:42:41 +0100

    chore(build): Add and configure postcss addon for storybook
    Fixes #888

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,5 +2,16 @@
 
 module.exports = {
 	stories: ['../packages/*/src/components/**/*.stories.@(js|jsx|mdx|tsx)'],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials']
+	addons: [
+		'@storybook/addon-links',
+		'@storybook/addon-essentials',
+		{
+			name: '@storybook/addon-postcss',
+			options: {
+				postcssLoaderOptions: {
+					implementation: require('postcss')
+				}
+			}
+		}
+	]
 };
