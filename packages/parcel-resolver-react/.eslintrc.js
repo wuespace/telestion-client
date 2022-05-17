6774caabb212aaca0e1eda95de56499bcea84400
Ludwig Richter <ludwig.richter@wuespace.de>
2022-05-17 18:27:59 +0200

    feat(resolver-react): Add custom react resolver to prevent multiple instances of react in build output in linked Telestion Projects

---
+++ packages/parcel-resolver-react/.eslintrc.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+	root: true,
+	parserOptions: {
+		tsconfigRootDir: __dirname,
+		project: [path.join(__dirname, 'tsconfig.json')]
+	},
+	extends: [
+		path.join(__dirname, '..', '..', 'base-configs', 'eslint.typescript.js')
+	],
+	overrides: [
+		{
+			files: ['**/tests/lib/**/*', '**/tests/samples/**/*'],
+			rules: {
+				'jest/no-export': 'off',
+				'import/no-extraneous-dependencies': 'off'
+			}
+		},
+		{
+			files: ['**/src/**/*.ts'],
+			rules: {
+				'import/no-default-export': 'off'
+			}
+		}
+	]
+};
